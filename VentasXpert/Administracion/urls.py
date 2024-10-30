bb4ec2762afa1531455245b032d29b049525211f
byChino <chinommg10ft@gmail.com>
2024-10-30 13:38:03 -0600

    La_Chingadera_de_Godinez

--- VentasXpert/Administracion/urls.py
+++ VentasXpert/Administracion/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.administrador_home, name='administrador_home'),
+    path('reportes/', views.reportes_home, name='reportes_home'),
+    path('inventario/', views.inventario_home, name='inventario_home'),
+     path('proovedores/', views.proovedores_home, name='proovedores_home'),
 ]
